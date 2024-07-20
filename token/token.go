47c4ed7757cff55cb1687a86936c4b4adf2b9c84
Hao Nguyen <hao@haonguyen.tech>
2024-07-20 17:00:02 +0700

    feat: add new operator and complex token

--- token/token.go
+++ token/token.go
@@ -14,8 +14,15 @@ const (
 	IDENT = "IDENT" // add, foobar, x, y, ...
 	INT   = "INT"
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
 	// Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
