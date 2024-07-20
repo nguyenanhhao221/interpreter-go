bc0c490de27e4cda4736a7bd5d9569632b85c6cc
Hao Nguyen <hao@haonguyen.tech>
2024-07-20 22:36:50 +0700

    feat(lexer): add more ident keyword
    - boolean, return ,if...

--- token/token.go
+++ token/token.go
@@ -34,11 +34,21 @@ const (
 	// 1343456
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // Cap first char to export this func
